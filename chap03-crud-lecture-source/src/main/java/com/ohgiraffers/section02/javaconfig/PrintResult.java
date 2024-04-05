2556e1b3ca8fa027d1085f6b98ed0b335de9dfa2
LEEMINKUK <158013587+LEEMINKUK@users.noreply.github.com>
2024-04-05 16:53:59 +0900

    commit

---
+++ chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/PrintResult.java
@@ -0,0 +1,64 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import java.util.List;
+
+public class PrintResult {
+    public void printMenuList(List<MenuDTO> menuList) {
+
+        for(MenuDTO menu : menuList){
+            System.out.println(menu);
+        }
+    }
+
+    public void printErrorMessage(String errorCode) {
+
+        String errorMessage = "";
+
+        switch (errorCode){
+            case "selectList" : errorMessage = "메뉴 목록 조회를 실패 하셨습니다.";
+            break;
+            case "selectOne" : errorMessage = "메뉴 상세 조회를 실패 하셨습니다.";
+            break;
+            case "insert" : errorMessage = "신규 메뉴 등록을 실패 하셨습니다.";
+            break;
+        }
+        System.out.println(errorMessage);
+    }
+
+    public void printMenu(MenuDTO menu) {
+
+        System.out.println(menu);
+    }
+
+    public void printSuccessMessage(String successCode) {
+
+        String successMessage ="";
+        switch (successCode){
+            case "insert" : successMessage = "신규 등록을 성공 했습니다";
+            break;
+        }
+        System.out.println(successMessage);
+
+    }
+
+    public void printModifyMessage(String modify) {
+
+        String modifyMessage = "";
+        switch (modify){
+            case "modify" : modifyMessage = "수정 완료 했습니다.";
+            break;
+        }
+        System.out.println(modifyMessage);
+
+    }
+
+    public void printDeleteMenuMessage(String delete) {
+
+        String deleteMessage = "";
+        switch (delete){
+            case "delete" : deleteMessage = "삭제 완료 했습니다.";
+                break;
+        }
+        System.out.println(deleteMessage);
+    }
+}
